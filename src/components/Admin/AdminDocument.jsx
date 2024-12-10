c4a504fa3d68a5a32f36fb5c49b6971e1c74913d
Jenni Febiyola Sari <halo.jenni01@gmail.com>
2024-12-10 22:01:10 +0700

    updated

--- src/components/Admin/AdminDocument.jsx
+++ src/components/Admin/AdminDocument.jsx
@@ -1,19 +1,49 @@
 import React, { useState } from 'react';
-import { FaPlus, FaTrash, FaSearch, FaEdit, FaEye } from 'react-icons/fa';
-import { LuArrowUpDown } from "react-icons/lu";
+import { FaPlus, FaTrash, FaSearch, FaEye, FaEdit } from 'react-icons/fa';
+import { LuArrowUpDown } from 'react-icons/lu';
 import AddNewDocument from './AddNewDocument';
+import ViewDocument from './ViewDocument';
+import EditDocument from './EditDocument';
 
 const AdminDocument = () => {
-  const [selectAll, setSelectAll] = useState(false); 
-  const [checkedItems, setCheckedItems] = useState([]); 
-  const [search, setSearch] = useState(''); 
-  const [statusFilter, setStatusFilter] = useState('');
+  const [selectAll, setSelectAll] = useState(false);
+  const [checkedItems, setCheckedItems] = useState([]);
+  const [search, setSearch] = useState('');
+  const [statusFilter, setStatusFilter] = useState("");
   const [showAddForm, setShowAddForm] = useState(false);
+  const [showViewModal, setShowViewModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [selectedDocument, setSelectedDocument] = useState(null);
 
   const data = [
-    { id: 1, document: "Document 1", category: "Document Procedur", description: 'Description 1', issueDate: '2024-01-01', revision: 'v1.0', status: 'Available',},
-    { id: 2, document: "Document 2", category: "Document Procedur", description: 'Description 2', issueDate: '2024-02-01', revision: 'v1.1', status: 'Borrowed', borrowedby: 'Jenni' },
-    { id: 3, document: "Document 3", category: "Document Procedur", description: 'Description 3', issueDate: '2024-03-01', revision: 'v2.0', status: 'Available',},
+    {
+      id: 1,
+      document: 'Document 1',
+      category: 'Document Procedur',
+      description: 'Description 1',
+      issueDate: '2024-01-01',
+      revision: 'v1.0',
+      status: 'Available',
+    },
+    {
+      id: 2,
+      document: 'Document 2',
+      category: 'Document Procedur',
+      description: 'Description 2',
+      issueDate: '2024-02-01',
+      revision: 'v1.1',
+      status: 'Borrowed',
+      borrowedby: 'Jenni',
+    },
+    {
+      id: 3,
+      document: 'Document 3',
+      category: 'Document Procedur',
+      description: 'Description 3',
+      issueDate: '2024-03-01',
+      revision: 'v2.0',
+      status: 'Available',
+    },
   ];
 
   const handleSelectAll = () => {
@@ -25,20 +55,31 @@ const AdminDocument = () => {
     }
   };
 
+  const filteredData =
+  statusFilter === "all" || statusFilter === ""
+    ? data // Tampilkan semua data
+    : data.filter((item) => item.status === statusFilter);
+
+
   const handleCheckboxChange = (id) => {
     if (checkedItems.includes(id)) {
-      setCheckedItems(checkedItems.filter((item) => item !== id)); 
+      setCheckedItems(checkedItems.filter((item) => item !== id));
     } else {
-      setCheckedItems([...checkedItems, id]); 
+      setCheckedItems([...checkedItems, id]);
     }
   };
 
-  const filteredData = data.filter((item) => {
-    const searchMatch = item.document.toLowerCase().includes(search.toLowerCase());
-    const statusMatch = statusFilter ? item.status === statusFilter : true;
-    return searchMatch && statusMatch;
-  });
+  const handleViewClick = (document) => {
+    setSelectedDocument(document);
+    setShowViewModal(true);
+  };
 
+  const handleEditClick = (document) => {
+    setSelectedDocument(document);
+    setShowEditModal(true);
+  };
+
+ 
   return (
     <div className="overflow-auto px-5">
       <div className="mb-4 flex justify-between items-center">
@@ -47,15 +88,17 @@ const AdminDocument = () => {
 
       <div className="flex justify-between mb-4">
         <button
-          className="px-4 py-2 bg-[#3a99ff] text-white source-sans-3-regular rounded"
+          className="px-4 py-2 bg-[#3a99ff] text-white source-sans-3-regular rounded-md transition-transform duration-200 active:scale-95 hover:opacity-90"
           onClick={() => setShowAddForm(true)}
         >
           <FaPlus className="inline mr-2" />
           Add New Document
         </button>
         <button
-          className={`px-4 py-2 rounded ${checkedItems.length === data.length ? 'bg-red-500' : 'bg-gray-500'} text-white`}
-          disabled={checkedItems.length !== data.length} // Tombol hanya aktif jika semua dicentang
+          className={`px-4 py-2 rounded ${
+            checkedItems.length === data.length ? 'bg-red-500' : 'bg-gray-500'
+          } text-white`}
+          disabled={checkedItems.length !== data.length}
         >
           <FaTrash className="inline mr-2" />
           Delete
@@ -67,54 +110,65 @@ const AdminDocument = () => {
           <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
           <input
             type="text"
-            className="pl-10 px-4 py-2 border border-gray-300 rounded-lg w-full"
+            className="pl-10 px-4 py-2 border border-gray-300 rounded-lg w-full focus:outline-none"
             placeholder="Search by document name"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
           />
         </div>
 
-        <select
-        className="px-4 py-2 border border-gray-300 rounded-lg w-1/4"
-        value={statusFilter}
-        onChange={(e) => setStatusFilter(e.target.value)}
-        >
-          <option value="" disabled selected hidden>
-            <LuArrowUpDown />
-            Sort by
-          </option>
-          <option value="">All Status</option>
-          <option value="Available">Available</option>
-          <option value="Borrowed">Borrowed</option>
-        </select>
+        <div className="relative w-1/4">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <LuArrowUpDown className="text-gray-500" />
+          </div>
+          <select
+            className="px-10 py-2 border border-gray-300 rounded-lg w-full appearance-none focus:outline-none cursor-pointer"
+            value={statusFilter}
+            onChange={(e) => setStatusFilter(e.target.value)}
+          >
+            {statusFilter === "" && (
+              <option value="" disabled hidden>
+                Sort by
+              </option>
+            )}
+            <option value="all">All Status</option>
+            <option value="Available">Available</option>
+            <option value="Borrowed">Borrowed</option>
+          </select>
+        </div>
+
       </div>
 
       <div className="overflow-x-auto">
-        <table className="min-w-full bg-white rounded-lg border border-gray-200">
-        <thead>
-          <tr>
-            <th className="px-4 py-2 border rounded-xl whitespace-nowrap">
-              <input
-                type="checkbox"
-                className="w-4 h-4"
-                checked={selectAll}
-                onChange={handleSelectAll}
-              />
-            </th>
-            <th className="px-4 py-2 border whitespace-nowrap">Document Name</th>
-            <th className="px-4 py-2 border whitespace-nowrap">Category</th>
-            <th className="px-4 py-2 border whitespace-nowrap">Description</th>
-            <th className="px-4 py-2 border whitespace-nowrap">Issue Date</th>
-            <th className="px-4 py-2 border whitespace-nowrap">Revision</th>
-            <th className="px-4 py-2 border whitespace-nowrap">Status</th>
-            <th className="px-4 py-2 border whitespace-nowrap">Borrowed By</th>
-            <th className="px-4 py-2 border sticky lg:sticky top-0 right-0 bg-white w-1/6 text-center whitespace-nowrap">Action</th>
-          </tr>
-        </thead>
+        <table className="min-w-full bg-white rounded-lg border border-gray-200 table-fixed">
+          <thead>
+            <tr>
+              <th className="w-20 px-4 py-2 border sticky top-0 left-0 bg-white rounded whitespace-nowrap">
+                <input
+                  type="checkbox"
+                  className="w-4 h-4"
+                  checked={selectAll}
+                  onChange={handleSelectAll}
+                />
+              </th>
+              <th className="w-44 px-4 py-2 border sticky top-0 left-10 bg-white whitespace-nowrap">
+                Document Name
+              </th>
+              <th className="w-40 px-4 py-2 border whitespace-nowrap">Category</th>
+              <th className="w-40 px-4 py-2 border whitespace-nowrap">Description</th>
+              <th className="w-40 px-4 py-2 border whitespace-nowrap">Issue Date</th>
+              <th className="w-40 px-4 py-2 border whitespace-nowrap">Revision</th>
+              <th className="w-40 px-4 py-2 border whitespace-nowrap">Status</th>
+              <th className="w-40 px-4 py-2 border whitespace-nowrap">Borrowed By</th>
+              <th className="w-40 px-4 py-2 border sticky top-0 right-0 bg-white text-center whitespace-nowrap">
+                Action
+              </th>
+            </tr>
+          </thead>
           <tbody>
             {filteredData.map((item) => (
               <tr key={item.id} className="border-t">
-                <td className="px-4 py-2 border">
+                <td className="w-20 px-4 py-2 border sticky top-0 left-0 bg-white border-r">
                   <input
                     type="checkbox"
                     className="w-4 h-4"
@@ -122,30 +176,38 @@ const AdminDocument = () => {
                     onChange={() => handleCheckboxChange(item.id)}
                   />
                 </td>
-                <td className="px-4 py-2 border text-center">{item.document}</td>
-                <td className="px-4 py-2 border text-center">{item.category}</td>
-                <td className="px-4 py-2 border text-center">{item.description}</td>
-                <td className="px-4 py-2 border text-center">{item.issueDate}</td>
-                <td className="px-4 py-2 border text-center">{item.revision}</td>
-                <td className="px-4 py-2 border">
+                <td className="w-40 px-4 py-2 border sticky top-0 left-10 bg-white border-r text-center">
+                  {item.document}
+                </td>
+                <td className="w-40 px-4 py-2 border text-center">{item.category}</td>
+                <td className="w-40 px-4 py-2 border text-center">{item.description}</td>
+                <td className="w-40 px-4 py-2 border text-center">{item.issueDate}</td>
+                <td className="w-40 px-4 py-2 border text-center">{item.revision}</td>
+                <td className="w-40 px-4 py-2 border text-center">
                   <span
                     className={`${
                       item.status === 'Available' ? 'text-green-500' : 'text-red-500'
                     } font-semibold`}
-                    >
+                  >
                     {item.status}
                   </span>
                 </td>
-                <td className="px-4 py-2 border text-center">{item.borrowedby}</td>
-                <td className="px-2 py-2 border sticky lg:sticky right-0 bg-white">
+                <td className="w-40 px-4 py-2 border text-center">{item.borrowedby}</td>
+                <td className="w-40 px-2 py-2 border sticky right-0 bg-white border-l text-center">
                   <div className="flex justify-around space-x-2 sm:space-x-4">
-                    <button className="text-white hover:text-blue-600 bg-gray-400 px-2 py-1 rounded">
+                    <button
+                      className="text-white bg-gray-400 px-2 py-1 rounded hover:bg-blue-600"
+                      onClick={() => handleViewClick(item)}
+                    >
                       <FaEye />
                     </button>
-                    <button className="text-white hover:text-blue-600 bg-gray-400 px-2 py-1 rounded">
+                    <button
+                      className="text-white bg-gray-400 px-2 py-1 rounded hover:bg-blue-600"
+                      onClick={() => handleEditClick(item)}
+                    >
                       <FaEdit />
                     </button>
-                    <button className="text-white hover:text-blue-600 bg-gray-400 px-2 py-1 rounded">
+                    <button className="text-white hover:bg-red-600 bg-gray-400 px-2 py-1 rounded">
                       <FaTrash />
                     </button>
                   </div>
@@ -155,7 +217,20 @@ const AdminDocument = () => {
           </tbody>
         </table>
       </div>
+
       {showAddForm && <AddNewDocument onClose={() => setShowAddForm(false)} />}
+      {showViewModal && (
+        <ViewDocument
+          document={selectedDocument}
+          onClose={() => setShowViewModal(false)}
+        />
+      )}
+      {showEditModal && (
+        <EditDocument
+          document={selectedDocument}
+          onClose={() => setShowEditModal(false)}
+        />
+      )}
     </div>
   );
 };
