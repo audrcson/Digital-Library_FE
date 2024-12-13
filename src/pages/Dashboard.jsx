3c19f022dd5e037e43672a6e37ff601ee64f592c
Jenni Febiyola Sari <halo.jenni01@gmail.com>
2024-12-13 21:59:42 +0700

    updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -13,7 +13,42 @@ const Dashbord = () => {
     "Document Procedure-MEM-DP-P602_4": [
       { doc: "MEM-DP-P602_4", desc: "Link 1", date: "a", rev: "b", fileUrl: "/10-DP-P602_4.pdf" },
     ],
-    // ... data lainnya
+    "Document Procedure-MEM-DP-W602_01": [
+      { doc: "MEM-DP-W602_01", desc: "Link 2", date: "a", rev: "b", fileUrl: "/10-DP-W602_01.pdf" },
+    ],
+    "Document Procedure-MEM-DP-W602_02": [
+      { doc: "MEM-DP-W602_02", desc: "Link 3", date: "a", rev: "b", fileUrl: "/10-DP-W602_02.pdf" },
+    ],
+    "Document Manual Book-MEM-MB-MAC": [
+      { doc: "MEM-MB-MAC", desc: "Link 1", date: "a", rev: "b" },
+    ],
+    "Document Manual Book-MEM-MB-MFT": [
+      { doc: "MEM-MB-MFT", desc: "Link 1", date: "a", rev: "b" },
+    ],
+    "Document Manual Book-MEM-MB-BCT": [
+      { doc: "MEM-MB-BCT", desc: "Link 1", date: "a", rev: "b" },
+    ],
+    "Document Manual Book-MEM-MB-SFT": [
+      { doc: "MEM-MB-SFT", desc: "Link 1", date: "a", rev: "b" },
+    ],
+    "Document Manual Book-MEM-MB-UTL": [
+      { doc: "MEM-MB-UTL", desc: "Link 1", date: "a", rev: "b" },
+    ],
+    "Document Manual Book-MEM-MB-GEN": [
+      { doc: "MEM-MB-GEN", desc: "Link 1", date: "a", rev: "b" },
+    ],
+    "Bisnis Proses-Maint. Engineering & Development": [
+      { doc: "Maint. Engineering & Development", desc: "Link 1", date: "a", rev: "b" },
+    ],
+    "Bisnis Proses-Maint. Planning & Control": [
+      { doc: "Maint. Planning & Control", desc: "Link 1", date: "a", rev: "b" },
+    ],
+    "Bisnis Proses-Maint. Service & Repair Machine": [
+      { doc: "Maint. Service & Repair Machine", desc: "Link 1", date: "a", rev: "b" },
+    ],
+    "Bisnis Proses-Metal Forming, Quality, Tooling": [
+      { doc: "Metal Forming, Quality, Tooling", desc: "Link 1", date: "a", rev: "b" },
+    ],
   };
 
   const handleHamburgerClick = () => {
